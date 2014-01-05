c66b4e92bd063ed791a5d74189eb4a032ad26405
kunigami <kunigami@fb.com>
2014-01-05 15:53:48 -0800

    reorganizing, group by topics, enabling column

--- init.el
+++ init.el
@@ -12,28 +12,25 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General editing preferences
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Show matching parent
 (show-paren-mode t)
 
 ;; Show trailing whitespace
 (set-face-background 'trailing-whitespace "#900000")
 (setq-default show-trailing-whitespace t)
+
 ;; Remove trailing whitespace upon saving
 (add-hook 'write-file-hooks 'delete-trailing-whitespace)
 
+;; Display column number
+(setq column-number-mode t)
+
 ;; Disable bip sound
 (setq ring-bell-function 'ignore)
-
-;; C-x o is not enough when we have more than two buffers openend
-;; because it only go forward. This adds C-x p to go backwards.
-(global-set-key (kbd "C-x p") 'previous-multiframe-window)
-
-;; Emacs 24 supports a basic set of themes
-(load-theme 'tango-dark)
-
-;; Custom settings for working with Agda programming language
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
 
 ;; uniquify.el is a helper routine to help give buffer names a better unique name
 (when (load "uniquify" 'NOERROR)
@@ -44,8 +41,41 @@
 ;; Keep the files in sync with disk
 (global-auto-revert-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Shortcuts
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; C-x o is not enough when we have more than two buffers openend
+;; because it only go forward. This adds C-x p to go backwards.
+(global-set-key (kbd "C-x p") 'previous-multiframe-window)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Styling
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Emacs 24 supports a basic set of themes
+(load-theme 'tango-dark)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packaging
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Use marmalade to manage packages
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Agda
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Custom settings for working with Agda programming language
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Haskell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ '(haskell-mode-hook '(turn-on-haskell-indentation)))
